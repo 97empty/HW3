985911d1a1d873205aad1a9c63dd5d7d35c185c7
97empty <quarety@gmail.com>
2022-10-17 19:21:47 +0300

    second commit

---
+++ src/FileHandler.java
@@ -0,0 +1,42 @@
+
+import com.geekbrains.model.CloudMessage;
+import com.geekbrains.model.FileMessage;
+import com.geekbrains.model.FileRequest;
+import com.geekbrains.model.ListMessage;
+import com.geekbrains.model.*;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@Slf4j
+public class FileHandler extends SimpleChannelInboundHandler<CloudMessage> {
+    private Path serverDir;
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        serverDir = Path.of("server_files");
+        serverDir = Path.of("server_files").toAbsolutePath();
+        ctx.writeAndFlush(new ListMessage(serverDir));
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, CloudMessage cloudMessage) throws Exception {
+        log.debug("Received: {}", cloudMessage.getType());
+        if (cloudMessage instanceof FileMessage fileMessage) {
+            Files.write(serverDir.resolve(fileMessage.getFileName()), fileMessage.getBytes());
+            ctx.writeAndFlush(new ListMessage(serverDir));
+        } else if (cloudMessage instanceof FileRequest fileRequest) {
+            ctx.writeAndFlush(new FileMessage(serverDir.resolve(fileRequest.getFileName())));
+        } else if (cloudMessage instanceof DirFileListRequest dirList){
+            if (new File(serverDir.toString() + File.separator + dirList.getDirName()).isDirectory()) {
+                serverDir = serverDir.resolve(dirList.getDirName()).normalize();
+                log.debug("serverDir: {}", serverDir);
+                ctx.writeAndFlush(new ListMessage(serverDir));
+            }
+        }
+    }
+}
